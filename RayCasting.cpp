836e1000ffadcf5715a505aee06dc66d388dfcc3
Joseph Prachar <josephprachar@comcast.net>
2015-03-10 00:06:44 -0700

    Added window input

--- RayCasting.cpp
+++ RayCasting.cpp
@@ -5,41 +5,74 @@
 using namespace std;
 
 void printHeader(ofstream* myFile, int width, int height);
-Sphere* readFile(std::string file, int* length);
+
+void readSetupFile(std::string file, Window* view, Point* eye, Color* ambient, Light* light);
+Sphere* readSphereFile(std::string file, int* length);
+
 Sphere parseSphere(std::string line, bool* good);
+Window parseWindow(std::string line, bool* good);
+Point parsePoint(std::string line, bool* good);
+Light parseLight(std::string line, bool* good);
+Color parseColor(std::string line, bool* good);
+vector<float> parseFloats(std::string line, int expected);
+
 std::vector<std::string> split(const std::string &s, char delim);
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Window view = Window(-10, 10, -7.5, 7.5, 1024, 768);
-	Point eye = Point(0,0,-14);
+	Window view = Window(0,0,0,0,0,0);
+	Point eye = Point(0,0,0);
+	Color ambientColor = Color(0,0,0);
+	Light pointLight = Light(Point(0,0,0), Color(0,0,0));
+
+	readSetupFile("setup.in", &view, &eye, &ambientColor, &pointLight);
+	
 	int length = 0;
-	Sphere* spheres = readFile("input.in", &length);
-	Color ambientColor = Color(1, 1, 1);
-	Light pointLight = Light(Point(-100, 100, -100), Color(1.5, 1.5, 1.5));
+	Sphere* spheres = readSphereFile("input.in", &length);
 
 	RayCaster rc = RayCaster(view, eye, spheres, length, ambientColor, pointLight);
 
 	ofstream myFile;
 	myFile.open("image.ppm");
-	cout<< length;
+
 	printHeader(&myFile, view.width, view.height);
 	rc.castAllRays(&myFile);
 
 	myFile.close();
-
 }
 
-void printHeader(ofstream* myFile, int width, int height){
+void printHeader(std::ofstream* myFile, int width, int height){
 	*myFile << "P3\n";
 	*myFile << width << ' ' << height;
 	*myFile << "\n255\n";
 }
 
-Sphere* readFile(std::string file, int* length){
-	std::vector<Sphere> list;
+void readSetupFile(std::string file, Window* view, Point* eye, Color* ambient, Light* light){
 	string line;
+	bool good = true;
+
+	std::ifstream f(file);
+
+	if (f.is_open()){
+		getline(f, line);
+		*view = parseWindow(line, &good);
+
+		getline(f, line);
+		*eye = parsePoint(line, &good);
+
+		getline(f, line);
+		*ambient = parseColor(line, &good);
+
+		getline(f, line);
+		*light = parseLight(line, &good);
+	}
+
+	f.close();
+}
+Sphere* readSphereFile(std::string file, int* length){
+	std::vector<Sphere> list;
+	std::string line;
 
 	std::ifstream f(file);
 	int count = 0;
@@ -66,21 +99,72 @@ Sphere* readFile(std::string file, int* length){
 }
 
 Sphere parseSphere(std::string line, bool* good){
-	vector<string> chars = split(line, ' ');
-	
-	if (chars.size() == 11){
-		float vals[11];
-		for (int i = 0; i < 11; ++i){
-			vals[i] = std::stof(chars[i]);
-			//cout << vals[i] << ' ';
-		}
-		//cout << "\n";
+	vector<float> vals = parseFloats(line, 11);
+	if (vals.size() != 0){
 		*good = true;
 		return Sphere(Point(vals[0], vals[1], vals[2]), vals[3], Color(vals[4], vals[5], vals[6]), Finish(vals[7], vals[8], vals[9], vals[10]));
 	}
-	
+
 	*good = false;
 	return Sphere();
+}
+Window parseWindow(std::string line, bool* good){
+	vector<float> vals = parseFloats(line, 6);
+	if (vals.size() != 0){
+		*good = true;
+		return Window(vals[0], vals[1], vals[2], vals[3], (int)vals[4], (int)vals[5]);
+	}
+
+	*good = false;
+	return Window(0,0,0,0,0,0);
+}
+Point parsePoint(std::string line, bool* good){
+	vector<float> vals = parseFloats(line, 3);
+	if (vals.size() != 0){
+		*good = true;
+		return Point(vals[0], vals[1], vals[2]);
+	}
+
+	*good = false;
+	return Point(0,0,0);
+}
+Light parseLight(std::string line, bool* good){
+		vector<float> vals = parseFloats(line, 6);
+	if (vals.size() != 0){
+		*good = true;
+		return Light(Point(vals[0], vals[1], vals[2]), Color(vals[3], vals[4], vals[5]));
+	}
+
+	*good = false;
+	return Light(Point(0,0,0), Color(0,0,0));
+}
+Color parseColor(std::string line, bool* good){
+	vector<float> vals = parseFloats(line, 3);
+	if (vals.size() != 0){
+		*good = true;
+		return Color(vals[0], vals[1], vals[2]);
+	}
+
+	*good = false;
+	return Color(0,0,0);
+}
+vector<float> parseFloats(std::string line, int expected){
+	vector<string> chars = split(line, ' ');
+	vector<float> nums = vector<float>();
+
+	if (chars.size() == expected){
+		try{
+			for (int i = 0; i < expected; ++i){
+				float f = std::stof(chars[i]);
+				nums.push_back(f);
+			}
+		}
+		catch (invalid_argument e){
+			return vector<float>();
+		}
+	}
+
+	return nums;
 }
 
 std::vector<std::string> split(const std::string &s, char delim) {
@@ -88,7 +172,6 @@ std::vector<std::string> split(const std::string &s, char delim) {
     split(s, delim, elems);
     return elems;
 }
-
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
